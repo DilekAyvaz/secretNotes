525029d800f3403460323fb39fb06624760654a6
DİLEK <dilekayvaz0@gmail.com>
2026-02-22 17:48:07 +0300

    save and encrypt added

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-from doctest import master
 from tkinter import *
+from tkinter import messagebox
+from turtle import end_fill
+
+
+def save_and_encrypt_notes():
+    title= title_entry.get()
+    message= input_text.get("1.0","end")
+    master_secret = master_secret_input.get()
+
+    if len(title)==0 or len(message)==0 or len(master_secret)==0:
+        messagebox.showwarning(title="Error!",message="Please enter your all info.")
+    else:
+        #encr
+        
+            with open("mysecret.txt" ,"a") as data_file:
+                data_file.write(f"\n{title}\n{message}")
+            title_entry.delete(0, "end")
+            input_text.delete("1.0", "end")
+            master_secret_input.delete(0,"end")
 
 
 FONT = ("Zapfino",20,"italic")
@@ -33,7 +51,7 @@ master_secret_label.pack()
 master_secret_input= Entry(width=20)
 master_secret_input.pack()
 
-save_button = Button(text="Save & Encrypt")
+save_button = Button(text="Save & Encrypt" , command=save_and_encrypt_notes)
 save_button.pack()
 
 decrypt_button =Button(text="Decrypt")
